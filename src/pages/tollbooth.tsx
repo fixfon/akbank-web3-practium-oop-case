a2af66d2909a8acec14232075f292d301890dace
fixfon <itsme@fixfon.dev>
2022-08-29 01:03:05 +0300

    Create toll booth tRPC routes and toll booth page

--- src/pages/tollbooth.tsx
+++ src/pages/tollbooth.tsx
@@ -6,10 +6,55 @@ import { useState } from 'react';
 import { TollBoothName } from '../lib/tollBooth/interfaces/ITollBooth';
 
 const TollBooths: NextPage = (props) => {
-	const [activeTollBooth, setActiveTollBooth] = useState<
-		TollBoothName | undefined
-	>(undefined);
+	const [selectedTollBooth, setSelectedTollBooth] =
+		useState<TollBoothName>('TollBooth1');
+	const [selectedCarValue, setSelectedCarValue] = useState<string>('none');
 
+	const findTollBoothQuery = trpc.useQuery(
+		[
+			'booth.findTollBooth',
+			{
+				tollBoothName: selectedTollBooth,
+			},
+		],
+		{
+			refetchOnReconnect: false,
+			refetchOnMount: false,
+			refetchOnWindowFocus: false,
+		}
+	);
+	const { data: findTollBoothQueryResult, refetch: findTollBoothRefetch } =
+		findTollBoothQuery;
+
+	const listTollBoothsQuery = trpc.useQuery(['booth.listTollBooths'], {
+		refetchOnReconnect: false,
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+	});
+	const { data: listTollBoothsQueryResult } = listTollBoothsQuery;
+
+	const carListQuery = trpc.useQuery(['car.listCars'], {
+		refetchOnReconnect: false,
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+	});
+	const { data: carListQueryResult, refetch: carListRefetch } = carListQuery;
+
+	const chargePassMutation = trpc.useMutation(['booth.chargePass'], {
+		onSuccess: () => {
+			findTollBoothRefetch();
+			carListRefetch();
+		},
+	});
+	const { mutate: mutateChargePass, data: chargePassResult, error: chargePassError } =
+		chargePassMutation;
+
+	const handleChargePass = () => {
+		mutateChargePass({
+			HGSId: Number(selectedCarValue),
+			tollBoothName: selectedTollBooth,
+		});
+	};
 	return (
 		<Layout>
 			<Head>
@@ -26,28 +71,84 @@ const TollBooths: NextPage = (props) => {
 					<p className='font-semibold pb-2'>Select a Toll Booth</p>
 					<select
 						onChange={(e) => {
-							setActiveTollBooth(e.target.value);
-							console.log(e.target.value);
+							setSelectedTollBooth(e.target.value as TollBoothName);
 						}}
 						defaultValue={'none'}
 						className='w-full p-2 border border-gray-400 rounded-lg'>
 						<option value='none' disabled hidden>
 							Select a toll booth
 						</option>
+						{listTollBoothsQueryResult &&
+							listTollBoothsQueryResult.tollBoothNameList.map(
+								(tollBoothName, index) => {
+									return (
+										<option key={index} value={tollBoothName}>
+											{tollBoothName}
+										</option>
+									);
+								}
+							)}
 					</select>
-					<p className='font-semibold pt-4 py-2'>Select Charged Car</p>
+					<div>
+						{findTollBoothQueryResult && (
+							<>
+								<div className='flex flex-row gap-3'>
+									<p className='font-semibold pt-4 pb-2'>Toll Booth Name: </p>
+									<p className='pt-4 pb-2'>
+										{findTollBoothQueryResult.tollBoothInfo.tollBoothName}
+									</p>
+								</div>
+								<div className='flex flex-row gap-3'>
+									<p className='font-semibold pb-2'>Balance: </p>
+									<p className='pb-2'>
+										{findTollBoothQueryResult.tollBoothInfo.balance}
+									</p>
+								</div>
+							</>
+						)}
+					</div>
+					<p className='font-semibold pt-4 py-2'>Select A Car to Charge</p>
 					<select
+						onChange={(e) => {
+							setSelectedCarValue(e.target.value);
+						}}
 						defaultValue={'none'}
 						className='w-full p-2 border border-gray-400 rounded-lg'>
 						<option value='none' disabled hidden>
 							Select a car
 						</option>
+						{carListQueryResult &&
+							carListQueryResult.carInfoList.map((carInfo, index) => {
+								return (
+									<option key={index} value={carInfo.HGSId}>
+										{`${index + 1} - ${carInfo.HGSId} - ${carInfo.carType}`}
+									</option>
+								);
+							})}
 					</select>
 					<button
 						type='button'
-						className='rounded-lg font-semibold border border-red-600 bg-red-600 text-white mt-4 p-2 w-full'>
+						disabled={
+							selectedCarValue === 'none' ||
+							findTollBoothQuery.isFetching ||
+							listTollBoothsQuery.isFetching ||
+							chargePassMutation.isLoading
+						}
+						onClick={handleChargePass}
+						className='rounded-lg font-semibold border border-red-600 bg-red-600 text-white mt-4 p-2 w-full disabled:bg-red-900 disabled:border-red-900 disabled:cursor-not-allowed'>
 						Charge
 					</button>
+					{chargePassResult?.success &&
+						(
+							<p className='text-center pb-2 text-green-600 font-semibold'>
+								{chargePassResult.success}
+							</p>
+						)}
+					{chargePassError && (
+						<p className='text-center pb-2 text-red-600 font-semibold'>
+							{chargePassError.message}
+						</p>
+					)}
 				</section>
 			</main>
 		</Layout>
--- src/server/router/tollBooth.ts
+++ src/server/router/tollBooth.ts
@@ -4,44 +4,64 @@ import { z } from 'zod';
 export const boothRouter = createRouter()
 	.query('findTollBooth', {
 		input: z.object({
-			tollBoothName: z.string(),
+			tollBoothName: z.enum(['TollBooth1', 'TollBooth2', 'TollBooth3']),
 		}),
-		resolve({ input }) {
+		resolve({ input, ctx }) {
+			const foundTollBooth = ctx.tollBoothInstanceList.find(
+				(tb) => tb.getName() === input.tollBoothName
+			);
+
+			if (!foundTollBooth) {
+				throw new Error(`TollBooth with name ${input.tollBoothName} not found`);
+			}
+
+			const tollBoothInfo = {
+				tollBoothName: foundTollBooth.getName(),
+				balance: foundTollBooth.getBalance(),
+			};
 			return {
-				greeting: `Hello ${input ?? 'world'} ${input.tollBoothName}`,
+				tollBoothInfo,
 			};
 		},
 	})
 	.query('listTollBooths', {
 		resolve({ ctx }) {
+			const tollBoothNameList = ctx.tollBoothInstanceList.map((tb) =>
+				tb.getName()
+			);
 			return {
-				greeting: `Hello world ${ctx.tollBoothInstanceList[0]?.balance}`,
+				tollBoothNameList: tollBoothNameList,
 			};
-		},
-	})
-	.query('getDailyRevenue', {
-		input: z.object({
-			tollBoothName: z.string(),
-		}),
-		resolve({ input }) {
-			return {
-				greeting: `Hello ${input ?? 'world'} ${input.tollBoothName}`,
-			};
-		},
-	})
-	.mutation('deleteTollBooth', {
-		input: z.object({
-			HGSId: z.number(),
-		}),
-		async resolve({ input, ctx }) {
-			return `${input}`;
 		},
 	})
 	.mutation('chargePass', {
 		input: z.object({
 			HGSId: z.number(),
+			tollBoothName: z.enum(['TollBooth1', 'TollBooth2', 'TollBooth3']),
 		}),
 		resolve({ input, ctx }) {
-			return `${input}`;
+			const foundTollBooth = ctx.tollBoothInstanceList.find(
+				(tb) => tb.getName() === input.tollBoothName
+			);
+
+			if (!foundTollBooth) {
+				throw new Error(`TollBooth with name ${input.tollBoothName} not found`);
+			}
+
+			const foundCar = ctx.carInstanceList.find(
+				(car) => car.HGSId === input.HGSId
+			);
+			if (!foundCar) {
+				throw new Error(`Car with HGSId ${input.HGSId} not found`);
+			}
+
+			foundTollBooth.chargePass(foundCar);
+			return {
+				success: `Car: ${foundCar.HGSId} was charged successfully at ${
+					foundTollBooth.name
+				} toll booth with ${foundTollBooth.getPassPrice(
+					foundCar.getCarType()
+				)}`,
+			};
 		},
 	});
